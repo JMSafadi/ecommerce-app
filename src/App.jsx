67c543bb6eb9ba9aa606bca8051d703e66b3f18d
Julian Safadi <julianmatiassafadi@hotmail.com>
2023-01-23 15:18:29 -0300

    context and custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,28 @@ import Payment from './containers/Payment'
 import Success from './containers/Success'
 import NotFound from './containers/NotFound'
 import Layout from './components/Layout'
+import AppContext from './context/AppContext'
+import useInitialState from './hooks/useInitialState'
 
 function App() {
 
+  const initialState = useInitialState()
+
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-            <Route exact path='/' element={<Home/>} />
-            <Route exact path='/checkout/' element={<Checkout/>} />
-            <Route exact path='/checkout/information' element={<Information/>} />
-            <Route exact path='/checkout/payment' element={<Payment/>} />
-            <Route exact path='/checkout/success' element={<Success/>} />
-            <Route exact element={<NotFound/>} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+              <Route exact path='/' element={<Home/>} />
+              <Route exact path='/checkout/' element={<Checkout/>} />
+              <Route exact path='/checkout/information' element={<Information/>} />
+              <Route exact path='/checkout/payment' element={<Payment/>} />
+              <Route exact path='/checkout/success' element={<Success/>} />
+              <Route exact element={<NotFound/>} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   )
 }
 
