ceb0ec4ff0dfbae8a0c48d70a67f0f1f9fb95832
Julian Safadi <julianmatiassafadi@hotmail.com>
2023-01-24 13:26:15 -0300

    Map API

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 import '../styles/Success.css'
+import Map from '../components/Map';
 
 const Success = () => {
+
+  const { state } = useContext(AppContext)
+  const { buyer } = state
+
   return (
     <div>
       <div className='Success'>
         <div className='Success-content'>
-          <h2>Oscar, gratis por tu compra</h2>
+          <h2>{`${buyer.name}, gratis por tu compra`}</h2>
           <span>Tu pedido llegara en 3 dias a tu direccion:</span>
           <div className='Success-map'>
-            Google Maps
+            <Map/>
           </div>
         </div>
       </div>
